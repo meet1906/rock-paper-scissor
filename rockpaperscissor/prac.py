4cc42a737e9e073bc44a3eb33a6ca8041986b526
meet1906 <meet19061999@gmail.com>
2020-03-29 18:09:11 +0530

    Basic rockpaperscissor

---
+++ rockpaperscissor/prac.py
@@ -0,0 +1,45 @@
+# Move the 3 following functions to utils.py
+def validate(hand):
+    if hand < 0 or hand > 2:
+        return False
+    return True
+
+def print_hand(hand, name='Guest'):
+    hands = ['Rock', 'Paper', 'Scissors']
+    print(name + ' picked: ' + hands[hand])
+
+def judge(player, computer):
+    if player == computer:
+        return 'Draw'
+    elif player == 0 and computer == 1:
+        return 'Lose'
+    elif player == 1 and computer == 2:
+        return 'Lose'
+    elif player == 2 and computer == 0:
+        return 'Lose'
+    else:
+        return 'Win'
+
+# import the utils module
+
+import random
+print('Starting the Rock Paper Scissors game!')
+player_name = input('Please enter your name: ')
+
+print('Pick a hand: (0: Rock, 1: Paper, 2: Scissors)')
+player_hand = int(input('Please enter a number (0-2): '))
+
+# Call the validate function of utils module
+if validate(player_hand):
+    computer_hand = random.randint(0,2)
+    
+    # Call the print_hand function of utils module
+    print_hand(player_hand, player_name)
+    print_hand(computer_hand, 'Computer')
+    
+    # Call the judge function of utils module
+    result = judge(player_hand, computer_hand)
+    print('Result: ' + result)
+else:
+    print('Please enter a valid number')
+
